f4ac20b85ef0874794f291a2b0df05c2ad57f2a9
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2019-01-09 12:20:26 +0700

    added test for generate css

---
+++ test/css_generate_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"cssgenerate"
+	"testing"
+)
+
+func TestGenerateCSS(t *testing.T) {
+	// sample css for font downloaded from google font
+	payload1, err := LoadTestData("sample_single_font.css")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// sample css for generated css based on css file location
+	payload2, err := LoadTestData("sample_single_font_generated_css.css")
+	if err != nil {
+		t.Error(err)
+	}
+	remoteURLs := []string{
+		"https://fonts.gstatic.com/s/lato/v14/S6uyw4BMUTPHjxAwXjeu.woff2", "https://fonts.gstatic.com/s/lato/v14/S6uyw4BMUTPHjx4wXg.woff2",
+	}
+	localPaths := []string{
+		"/home/fonts/S6uyw4BMUTPHjxAwXjeu.woff2",
+		"/home/fonts/S6uyw4BMUTPHjx4wXg.woff2",
+	}
+	destinationCSS := "/home/css/style.css"
+	actual, err := cssgenerate.GenerateCSS(payload1, remoteURLs, localPaths, destinationCSS)
+	if err != nil {
+		t.Error(err)
+	}
+	if actual != payload2 {
+		t.Error("Actual generated CSS doesn't match")
+		return
+	}
+}
